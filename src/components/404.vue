5ce4625f5e71a239c59c43518ff1ce1d57447dd7
lmh <495453865@qq.com>
2017-11-08 15:27:28 +0800

    404界面完成

--- src/components/404.vue
+++ src/components/404.vue
@@ -1,19 +1,20 @@
 <template>
-  <div>
-    404
+  <div class="noFound-wrapper">
+    <div class="noFound">
+      <img src="/static/404.jpg" alt="">
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  data () {
-    return {
-    }
-  }
-}
-</script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang='scss' scoped>
-
+  .noFound-wrapper{
+    min-height: calc(100% - 162px);
+    box-sizing: border-box;
+    padding-top: 50px;
+    img {
+      display: block;
+      margin: 0 auto;
+    }
+  }
 </style>
